5da1717d2b2ca888b966152e0c7f0766b586cede
Eduardo Mohedano <egarciamohedano@gmail.com>
2024-07-17 15:13:02 -0600

    Anadiendo implementacion de enqueue

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 //la operacion dequeue "que nos devolvera el primer elemento del queue y ademas borrara dicho elemento"
 //la ultima funcion print_queue() nos servira para visualizar el contenido del queue
 #include<stdio.h>
+#include<stdlib.h>
 
 
 //Anadiendo declaracion de funciones y estructura para implementar el QUEUE
@@ -22,5 +23,32 @@ int main(){
     printf("Un queue es como la fila del super...\n");
     printf("Es una estructura FIFO es decir, el primer elemento en entrar es el primero en salir...\n");
 
+    //Creando queue
+    Node* my_queue = NULL;
+    enqueue(&my_queue, 0);
+    enqueue(&my_queue, 1);
+    enqueue(&my_queue, 2);
+    enqueue(&my_queue, 3);
+
     return 0;
+}
+
+
+void enqueue(Node** front, int val){
+    if(*front == NULL){
+        *front = (Node*)malloc(sizeof(Node));
+        (*front)->val = val;
+        (*front)->next = NULL;
+    }
+    else{
+        Node* tmp = *front;
+        Node* last = NULL;
+        while( tmp != NULL){
+            last = tmp;
+            tmp = tmp->next;
+        }
+        last->next = (Node*)malloc(sizeof(Node));
+        last->next->val = val;
+        last->next->next = NULL;
+    }
 }
